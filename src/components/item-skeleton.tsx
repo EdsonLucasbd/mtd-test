4fe1984fe7f77db8012bc60b45bd2008f6d09c09
Edson Lucas <edsonlucas.lucas@gmail.com>
2024-09-26 10:41:54 -0300

    style: skeleton desktop size

--- src/components/item-skeleton.tsx
+++ src/components/item-skeleton.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from './ui/skeleton'
 
 export const ItemSkeleton = () => {
   return (
-    <div className='w-full lg:w-[350px]'>
-      <Skeleton className='h-[350px]' />
+    <div className='w-full lg:w-[280px]'>
+      <Skeleton className='h-[350px] lg:[250px]' />
     </div>
   )
 }
