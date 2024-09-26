3b65d99a45289f2289b6404dadbe76d69de8abbf
Edson Lucas <edsonlucas.lucas@gmail.com>
2024-09-26 09:35:36 -0300

    feat: using localStorage to persist items

--- src/store/cart-store.ts
+++ src/store/cart-store.ts
@@ -1,5 +1,6 @@
 import type { Image } from '@/types/menu'
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 export interface Product {
   name: string
@@ -16,51 +17,67 @@ interface CartState {
   clearCart: () => void
 }
 
+export interface StorageState {
+  state: {
+    products: Product[]
+    total: number
+    version: number
+  }
+}
+
 const calculateTotal = (products: Product[]) =>
   products.reduce((sum, product) => sum + product.price * product.quantity, 0);
 
-export const useCartStore = create<CartState>((set) => ({
-  products: [],
-  total: 0,
-  addToCart: (newProduct) => set((state) => {
-    const existingProduct = state.products.find((product) => product.name === newProduct.name)
+export const useCartStore = create<CartState>()(
+  persist(
+    (set) => ({
+      products: [],
+      total: 0,
+      addToCart: (newProduct) => set((state) => {
+        const existingProduct = state.products.find((product) => product.name === newProduct.name)
 
-    let updatedProducts
-    if (existingProduct) {
-      updatedProducts = state.products.map((product) => product.name === newProduct.name
-        ? { ...product, quantity: product.quantity + newProduct.quantity }
-        : product
-      )
-    } else {
-      updatedProducts = [...state.products, newProduct]
+        let updatedProducts
+        if (existingProduct) {
+          updatedProducts = state.products.map((product) => product.name === newProduct.name
+            ? { ...product, quantity: product.quantity + newProduct.quantity }
+            : product
+          )
+        } else {
+          updatedProducts = [...state.products, newProduct]
+        }
+
+        return {
+          products: updatedProducts,
+          total: calculateTotal(updatedProducts)
+        }
+      }),
+
+      removeFromCart: (name) => set((state) => {
+        const existingProduct = state.products.find((product) => product.name === name)
+        let updatedProducts = state.products
+
+        if (existingProduct?.quantity === 1) {
+          updatedProducts = state.products.filter((product) => product.name !== name)
+        } else if (existingProduct && existingProduct.quantity > 1) {
+          updatedProducts = state.products.map((product) => product.name === name
+            ? { ...product, quantity: product.quantity - 1 }
+            : product
+          )
+        }
+        return {
+          products: updatedProducts,
+          total: calculateTotal(updatedProducts)
+        }
+      }),
+
+      clearCart: () => set(() => ({
+        products: [],
+        total: 0
+      }))
+    }),
+    {
+      name: 'mtd-cart',
+      storage: createJSONStorage(() => localStorage),
     }
-
-    return {
-      products: updatedProducts,
-      total: calculateTotal(updatedProducts)
-    }
-  }),
-
-  removeFromCart: (name) => set((state) => {
-    const existingProduct = state.products.find((product) => product.name === name)
-    let updatedProducts = state.products
-
-    if (existingProduct?.quantity === 1) {
-      updatedProducts = state.products.filter((product) => product.name !== name)
-    } else if (existingProduct && existingProduct.quantity > 1) {
-      updatedProducts = state.products.map((product) => product.name === name
-        ? { ...product, quantity: product.quantity - 1 }
-        : product
-      )
-    }
-    return {
-      products: updatedProducts,
-      total: calculateTotal(updatedProducts)
-    }
-  }),
-
-  clearCart: () => set(() => ({
-    products: [],
-    total: 0
-  }))
-}))
+  )
+)
